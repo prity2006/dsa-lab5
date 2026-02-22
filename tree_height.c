4b8b0ad1b70b92627e023a91b1efb037048d1dcd
Prity Mukherjee <mukherjeeprity38@gmail.com>
2026-02-22 20:20:56 +0530

    new code updated

---
+++ tree_height.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure of a tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Function to calculate the height of the tree
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;  // Empty tree has height 0
+
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+}
+
+int main() {
+    // Creating the tree
+    //        1
+    //       / \
+    //      2   3
+    //     / \
+    //    4   5
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("Height of the tree: %d\n", height(root));
+
+    return 0;
+}
